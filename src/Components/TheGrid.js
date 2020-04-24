d143ae2d23e3e408e80f95f598bf6bcf9686d645
Robert Beckett <robert.beckett@onion.red>
2020-04-23 18:58:29 -0700

    Created array that will track grid state, now resizing along with the visual grid resizing.

--- src/Components/TheGrid.js
+++ src/Components/TheGrid.js
@@ -1,33 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './TheGrid.css';
 import GridItem from './GridItem';
 
 function TheGrid({ dimensions, mode }) {
-  function placeStart() {
+  const [theGrid, changeTheGrid] = useState(
+    () => {
+      // Just for fun.  Probably better to just use
+      //  a for loop.
+      let arr = [];
+      arr.length = (dimensions * dimensions);
+      arr.fill(0);
+      return arr;
+    }
+  );
 
-  }
-
-  function placeEnd() {
-
-  }
-
-  function placeObstacle() {
-
-  }
+  useEffect(() => {
+    const newGrid = theGrid.slice();
+    newGrid.length = dimensions * dimensions;
+    for (let i = 0; i < dimensions * dimensions; i++) {
+      // Comparing strict equals to undefined or
+      //  null was not working, so comparing typeof
+      //  instead.
+      if (typeof newGrid[i] === 'undefined') {
+        newGrid[i] = 0;
+      }
+    }
+    // Still need to maintain location of obstacles
+    //  and such when resizing.
+    // Find the row everything is on, find the offset
+    //  from the left, and move it back to that row with
+    //  that offset.
+    // For now, if the cell is removed due to shrinking,
+    //  just forget that obstacle.
+    changeTheGrid(newGrid);
+  }, [dimensions]);
 
   const clickFunc = {
-    0: placeObstacle,
-    1: placeStart,
-    2: placeEnd
+    0: mode => {
+      // We could use function currying.
+      changeTheGrid(theGrid)
+      return "obstacle";
+    },
+    1: mode => {
+      return "start";
+    },
+    2: mode => {
+      return "end";
+    }
   }
   
   function generateGrid() {
     const gridItems = [];
     const root = document.documentElement;
-    
+
     root.style.setProperty('--rowNum', dimensions);
     root.style.setProperty('--colNum', dimensions);
     for (let i = 0; i < dimensions * dimensions; i++) {
+      
       gridItems.push(
         <GridItem 
           id={i} 
